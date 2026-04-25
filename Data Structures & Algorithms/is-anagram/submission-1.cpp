9bdfa856cad0905c051354cff79992b48d524b56
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2026-04-25 09:03:41 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        
+        vector<int>v(26,0);
+
+        for(auto it:s){
+            v[it-'a']++;
+        }
+
+        for(auto it:t){
+           if(v[it-'a']<=0)return false;
+           v[it-'a']--;
+        }
+
+        
+        return true;
+    }
+};
