5d105e7323133efe10191c0b11bcc5e25e02122b
MayankRajAnand <74301715+MayankRajAnand@users.noreply.github.com>
2026-04-25 11:13:06 +0530

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string lcp(string a,string b){
+        string ans;
+        for(int i=0;i<min(a.size(),b.size());i++){
+            if(a[i]==b[i])ans+=a[i];
+            else return ans;
+        }
+        return ans;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())return "";
+        string ans=strs[0];
+        for(int i=1;i<strs.size();i++){
+            ans=lcp(ans,strs[i]);
+            if(ans=="")return "";
+        }
+        return ans;
+    }
+};
